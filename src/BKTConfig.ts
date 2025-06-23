52e7835fe902681cecd50b1ea0aa309a93a3fa83
duyhungtnn <duyhung71089@gmail.com>
2025-06-23 10:34:57 +0700

    chore: add comment

--- src/BKTConfig.ts
+++ src/BKTConfig.ts
@@ -6,6 +6,10 @@ import { SDK_VERSION } from './version';
 function defineBKTConfigForReact(config: RawBKTConfig): BKTConfig {
   return defineBKTConfig({
     ...config,
+    // Override wrapper SDK fields for React
+    // These fields are required for the React SDK to function correctly
+    // They will be overridden if provided in the input config
+    // to make sure we use the correct source ID and version
     wrapperSdkSourceId: SOURCE_ID_REACT,
     wrapperSdkVersion: SDK_VERSION,
     userAgent: `Bucketeer React SDK(${SDK_VERSION})`,
